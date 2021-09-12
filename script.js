0d73bc37c8e9e4b4306563276c3c9a40771cddc9
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-09-12 22:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,43 @@ const Followers = document.querySelector(".followers");
 const Following = document.querySelector(".following");
 
 
+// Search Button
 searchButton.onclick = () => {
+
+    // Entered Username
     let userName = searchBox.value.toLowerCase();
+
+    // Required url
     let url = 'https://api.github.com/users/' + userName;
 
+    // Fetching data through axios
     axios.get(url)
         .then(function(res) {
+
+            //Whole data
             console.log(res);
 
+            //Repositries Count
             let totalRepo = res.data.public_repos;
             RepoCount.innerHTML = totalRepo;
 
+            //Followers Count
             let followersCount = res.data.followers;
             Followers.innerHTML = followersCount;
 
+            //Following Count
             let followingCount = res.data.following;
             Following.innerHTML = followingCount;
         })
-        .catch(function(err) {
-            console.log(err);
-            alert("Invalid Username!");
-        });
 
+    //Error
+    .catch(function(err) {
+        console.log(err);
+
+        //If username is incorrect
+        alert("Invalid Username!");
+    });
+
+    //Re-initialise the search box to empty string
     searchBox.value = "";
 }
